1cc17214b1e1c24a8ada365afb42935298072fee
Radomir Vladislavić <vladislavic.rl@gmail.com>
2022-03-06 20:31:03 -0400

    implemented middle-square method

---
+++ src/utils/algorithms.js
@@ -0,0 +1,34 @@
+export const cuadradosMedios = (xi, limit) => {
+    const result = [];
+
+    const digits = String(xi).length;
+
+    for (let i = 1; i <= limit; i++) {
+        let row = [];
+
+        row.push(i, xi);
+
+        let yi = Math.pow(xi, 2);
+
+        let stringXi = String(yi);
+        if ((stringXi.length - digits) % 2 === 1) {
+            stringXi = "0" + stringXi;
+        }
+        row.push(stringXi);
+
+        let startIndex = (stringXi.length - digits) / 2;
+        xi = Number(stringXi.substring(startIndex, startIndex + digits));
+        row.push(xi);
+
+        let ri = xi / Math.pow(10, digits);
+        row.push(ri);
+        result.push(row);
+    }
+    return result;
+};
+
+export const productosMedios = () => {};
+
+export const lineal = () => {};
+
+export const multiplicativo = () => {};
